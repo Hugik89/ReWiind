ef1be3c3bcab6c1f58fae06bd5b38ff659e97f6b
Hugo KRET-ALLAVOINE <hugo.kret@gmail.com>
2024-05-28 12:47:49 +0200

    Slight header printing improvement

--- src/ast/astheader.cpp
+++ src/ast/astheader.cpp
@@ -25,13 +25,13 @@ void ASTHeader::printContents()
 {
     std::cout << "# AST header contents" << std::endl;
     std::cout << "Magic number : " << this->getMagic() << std::endl;
-    std::cout << "File size (bytes): " << this->getFileSize() << std::endl;
+    std::cout << "File size : " << this->getFileSize() << " bytes" << std::endl;
     std::cout << "Audio format : " << this->parseAudioFormat() << std::endl;
     std::cout << "Bit depth : " << this->getBitDepth() << std::endl;
     std::cout << "Number of channels : " << this->getChannelCount() << std::endl;
-    std::cout << "Sample rate (Hz) : " << this->getSampleRate() << std::endl;
+    std::cout << "Sample rate : " << this->getSampleRate() << " Hz" << std::endl;
     std::cout << "Total number of samples : " << this->getSampleCount() << std::endl;
     std::cout << "Loop start (sample count) : " << this->getLoopStart() << std::endl;
     std::cout << "Loop end (sample count) : " << this->getLoopEnd() << std::endl;
-    std::cout << "First block size (bytes) : " << this->getFirstBlockSize() << std::endl;
+    std::cout << "First block size : " << this->getFirstBlockSize() << " bytes" << std::endl;
 }
